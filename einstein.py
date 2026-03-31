4fd71c1fead75fb1610e911e819395e865db8b42
Deniz Dionne <denizdionne10@gmail.com>
2026-03-31 17:35:09 +0800

    Add comments to Lecture 0 problem sets, removed notes file, and edited einstein.py

--- einstein.py
+++ einstein.py
@@ -1,3 +1,11 @@
-m = int(input("Enter Mass in kg and integer\nm = "))
+"""
+In a file called einstein.py,
+implement a program in Python that prompts the user
+for mass as an integer (in kilograms) and then
+outputs the equivalent number of Joules as an integer.
+Assume that the user will input an integer.
+"""
+
+m = int(input("m = "))
 c = 300000000
 print(f"E = {m * c**2}")
--- playback.py
+++ playback.py
@@ -1,1 +1,9 @@
+""""
+In a file called playback.py,
+implement a program in Python
+that prompts the user for input
+and then outputs that same input,
+replacing each space with ... (i.e., three periods).
+"""
+
 print(input().replace(' ', "..."))
